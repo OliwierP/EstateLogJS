9c9005fbf5478d1351dc08725c4a2f2628251ac9
OliwierP <oliwier.pszeniczko@smcebi.edu.pl>
2025-05-30 11:00:52 +0200

    Fix DB connections

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:14
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
-COPY . .
+COPY ./app.js .
+COPY ./public ./public
 EXPOSE 3000
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.static('public'));
+
 
 const metricsMiddleware = promBundle({
   includeMethod: true,      
